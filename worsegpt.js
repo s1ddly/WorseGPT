d4f6a62212a1e3ca806af264d05a28c9b8e426d4
Sid Shardanand <51441497+s1ddly@users.noreply.github.com>
2024-06-17 20:49:57 +1000

    added gpt init

--- worsegpt.js
+++ worsegpt.js
@@ -1,3 +1,39 @@
 //----------------------------------------------------
 // The file for worseGPT
 //----------------------------------------------------
+convo = {}
+function gpt_init(model="gpt-4o", maxTokens="4000", temperature="1", topP="1", presencePenalty="0", frequencyPenalty="0", systemPrompt="You are ChatGPT, a large language model trained by OpenAI.\nCarefully heed the user's instructions.\nRespond using Markdown."){
+	convo = {
+		"model": "gpt-4o",
+		"messages": [
+			{
+				"role": "system",
+				"content": systemPrompt
+			},
+			{
+				"role": "user",
+				"content": "Hello!"
+			},
+			{
+				"role": "assistant",
+				"content": "yo whats cracking"
+			},
+			{
+				"role": "user",
+				"content": "not much B, whats 9 plus 10?"
+			}
+		],
+		"config": {
+			"max_tokens": maxTokens,
+			"model": model,
+			"temperature": temperature,
+			"presence_penalty": presencePenalty,
+			"top_p": topP,
+			"frequency_penalty": frequencyPenalty
+		}
+	}
+}
+
+function gpt_send(){
+	
+}
